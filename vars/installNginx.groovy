9146186d62800f55abc3f4cd0d0e37b44ea8a132
Abdalla Abdelsalam <51873396+abdalla-abdelsalam@users.noreply.github.com>
2023-09-22 14:17:42 +0300

    Update installNginx.groovy

--- vars/installNginx.groovy
+++ vars/installNginx.groovy
@@ -3,8 +3,4 @@ def call(String workerNode) {
         "sudo apt-get update",
         "sudo apt-get install nginx -y"
     ]
-    def result = bat(script: commands.join(" && "), label: "Install Nginx", returnStatus: true)
-    if (result != 0) {
-        error("Failed to install Nginx on $workerNode")
-    }
 }
