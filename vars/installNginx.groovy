d4fef3ca9c9ab7113a89dcd043a9c103d04b63f0
Abdalla Abdelsalam <51873396+abdalla-abdelsalam@users.noreply.github.com>
2023-09-22 14:25:10 +0300

    Update installNginx.groovy

--- vars/installNginx.groovy
+++ vars/installNginx.groovy
@@ -1,6 +1,30 @@
 def call(String workerNode) {
-    def commands = [
-        "sudo apt-get update",
-        "sudo apt-get install nginx -y"
-    ]
+     pipeline {
+    
+    agent {
+      label  'vm'
+    }
+
+    stages {
+        
+        stage('install nginx') {
+            steps {
+               sh 'sudo yum install nginx -y'
+            }
+        }
+        stage('start nginx') {
+            steps {
+                sh "sudo systemctl enable nginx "
+                 sh "sudo systemctl start nginx "
+            }    
+        }
+       stage('test nginx') {
+            steps {
+                sh "curl localhost "
+            }    
+        }
+       
+    }
+  
+}
 }
