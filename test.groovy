4c904aefe9174d8f6d4c9552272bc8dcb486d335
Abdalla Abdelsalam <51873396+abdalla-abdelsalam@users.noreply.github.com>
2023-09-22 13:51:50 +0300

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,10 +1,10 @@
-// SharedLibrary/src/com/example/NginxInstaller.groovy
-
-package com.example
-
-def installNginx(remoteHost) {
-    sh """
-        ssh ${remoteHost} 'sudo apt-get update'
-        ssh ${remoteHost} 'sudo apt-get install nginx -y'
-    """
+def call(String workerNode) {
+    def commands = [
+        "sudo apt-get update",
+        "sudo apt-get install nginx -y"
+    ]
+    def result = bat(script: commands.join(" && "), label: "Install Nginx", returnStatus: true)
+    if (result != 0) {
+        error("Failed to install Nginx on $workerNode")
+    }
 }
